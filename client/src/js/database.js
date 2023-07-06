e1378c3260c331c149104e298a608fe4324e77ac
renkayla <116777951+renkayla@users.noreply.github.com>
2023-07-06 13:06:31 -0400

    implemented the logic for the putDb and getDb methods to handle storing and retrieving content from the IndexedDB database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.clear(); // Optional: Clear the existing content before adding new content
+  await store.put({ content });
+  await tx.complete;
+  db.close();
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const content = await store.get(1);
+  await tx.complete;
+  db.close();
+  return content?.content;
+};
 
 initdb();
